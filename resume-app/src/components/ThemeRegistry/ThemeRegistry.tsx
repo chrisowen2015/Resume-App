4c6ebec92c4e68c1b3664977a16dd1ee5157ab9f
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2023-09-07 18:24:42 -0700

    Installed MUI Theme Components
    Installed MUI components from sample linked in comments

---
+++ resume-app/src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -0,0 +1,19 @@
+// Taken from https://github.com/mui/material-ui/blob/master/examples/material-next-app-router-ts/src/components/ThemeRegistry/ThemeRegistry.tsx
+'use client';
+
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import NextAppDirEmotionCacheProvider from './EmotionCache';
+import { lightTheme, darkTheme } from './theme';
+
+export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
